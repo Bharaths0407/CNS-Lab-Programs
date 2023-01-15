79c5126e4a9330a8dda4f2da122a3f4b5185a747
Alfazal007 <alfazal7777@gmail.com>
2023-01-15 20:14:14 +0530

    made some changes in tcp client

--- TCP Socket/client.java
+++ TCP Socket/client.java
@@ -1,14 +1,17 @@
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
 public class client {
     public static void main(String[] args) throws IOException {
         // connect to the server on localhost at port 7777
         Socket cs = new Socket("localhost", 7777); // cs = client socket
         DataOutputStream out = new DataOutputStream(cs.getOutputStream());
-        
+        System.out.println("Enter the file name : ");
+        Scanner sc = new Scanner(System.in);
+        String file = sc.next();
         // send the file name to the server
-        out.writeUTF("arjun.txt");
+        out.writeUTF(file);
         DataInputStream in = new DataInputStream(cs.getInputStream());
         
         // read the file data from the server
