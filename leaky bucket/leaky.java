f00f8d5b41066b4d199cdc0de5e5f4fc827f4bc0
Alfazal007 <alfazal7777@gmail.com>
2023-01-26 13:10:17 +0530

    leaky committed

--- leaky bucket/leaky.java
+++ leaky bucket/leaky.java
@@ -1,47 +1,43 @@
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
-class leaky {
-    public static void main(String args[]) {
-        int drop = 0, mini, nsec, p_remain = 0 , o_rate, b_size, i, packet[];
-        packet = new int[100];
+public class leaky {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the Bucket Size: ");
-        b_size = sc.nextInt();
-        System.out.println("Enter the Output Rate of Bucket: ");
-        o_rate = sc.nextInt();
-        System.out.println("Enter the Number of Seconds you want to simulate: ");
-        nsec = sc.nextInt();
+        System.out.println("Enter the bucket size : ");
+        int bSize = sc.nextInt();
+        System.out.println("Enter the output rate : ");
+        int oRate = sc.nextInt();
+        System.out.println("Enter the number of seconds to simulate ;");
+        int nSec = sc.nextInt();
         Random r = new Random();
-        for (i = 0; i < nsec; i++) {
-            packet[i] = ((r.nextInt(9) + 1) * 10);
+        int packet[] = new int[100];
+        for(int i = 0; i < nSec ; i++) {
+            packet[i] = (r.nextInt(9) + 1)* 10;
         }
-        System.out.println("Seconds || Packets Received || Packets Sent || Packets Left || Packet Dropped");
-        System.out.println(".............................................................................");
-        for (i = 0; i < nsec; i++) {
-            p_remain += packet[i];
-            if (p_remain > b_size) {
-                drop = p_remain - b_size;
-                p_remain = b_size;
-                System.out.print(i + 1 + "     " + packet[i] + "     ");
-                mini = Math.min(p_remain, o_rate);
-                System.out.print(mini + "     ");
-                p_remain -= mini;
-                System.out.print(p_remain + "     " + drop + "     " );
-                System.out.println();
-                drop = 0;
+        System.out.println("Seconds | Received | Sent | Remain | Dropped");
+        int drop = 0, remain = 0, mini = 0;
+        for(int i = 0; i < nSec; i++) {
+            remain = remain + packet[i];
+            System.out.print(i+ 1 + "  " + packet[i] + "  ");
+            if(remain > bSize) {
+                drop = remain - bSize;
+                remain = bSize;
             }
-        }
-        while (p_remain != 0) {
-            if (p_remain > b_size) {
-                drop = p_remain - b_size;
-                p_remain = b_size;
-            }
-            mini = Math.min(p_remain, o_rate);
-            System.out.print("     " + p_remain + "     " + mini + "     ");
-            p_remain -= mini;
-            System.out.println(p_remain + "     " + drop);
+            mini = Math.min(remain, oRate);
+            remain -= mini;
+            System.out.println(mini + "  " + remain + "  " + drop);
             drop = 0;
+        }
+        System.out.println();
+        while(remain != 0) {
+            drop = 0;
+            if(remain > bSize) {
+                drop = remain - bSize;
+                remain = bSize;
+            }
+            mini = Math.min(remain, oRate);
+            remain -= mini;
+            System.out.println("Sent = " + mini + "    Remaining = " + remain + "   Dropped = " + drop);
         }
     }
 }
